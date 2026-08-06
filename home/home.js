ee0087259f51493e06c8224bb78733a349b4d00e
CyberStudios404 <tristanbernier404@gmail.com>
2026-08-05 19:03:46 -0600

    Update home.js

--- home/home.js
+++ home/home.js
@@ -1,1 +1,34 @@
+// ==========================================================
+// Tristan Bernier Portfolio
+// Landing Page
+// home/home.js
+// ==========================================================
 
+const interactiveSurfaces = document.querySelectorAll(".portal-surface");
+
+interactiveSurfaces.forEach(surface => {
+
+    surface.addEventListener("pointermove", (e) => {
+
+        const rect = surface.getBoundingClientRect();
+
+        surface.style.setProperty(
+            "--mx",
+            `${e.clientX - rect.left}px`
+        );
+
+        surface.style.setProperty(
+            "--my",
+            `${e.clientY - rect.top}px`
+        );
+
+    });
+
+    surface.addEventListener("pointerleave", () => {
+
+        surface.style.setProperty("--mx", "50%");
+        surface.style.setProperty("--my", "50%");
+
+    });
+
+});
